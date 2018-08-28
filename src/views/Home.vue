48dcfa198be03312664fd29e2d79740e3a7f8a27
Natalie <natalie.todd15@gmail.com>
2018-08-27 20:13:21 -0600

    header working

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <router-view></router-view>
+    <Header />
+    <Options />
+</div>
+</template>
+    
+<script>
+import Header from '@/components/Header';
+import Options from '@/components/Options';
+export default {
+    components: {
+        Header,
+        Options
+    }
+};
+</script>
+
+<style>
+</style>
