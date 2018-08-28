ca9a0709413e684df96d8842f4e7b88e24d332a9
Natalie <natalie.todd15@gmail.com>
2018-08-27 23:17:36 -0600

    added form to AddBook

---
+++ src/views/book/NewBook.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <router-view></router-view>
+    <Header />
+    <Nav />
+    <AddBook />
+</div>
+</template>
+    
+<script>
+import Header from '@/components/Header';
+import Nav from '@/components/Nav';
+import AddBook from '@/components/AddBook';
+export default {
+    components: {
+        Header,
+        Nav,
+        AddBook
+    }
+};
+</script>
+
+<style>
+</style>
