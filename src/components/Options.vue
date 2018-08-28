8aaf482499fc6efcc0da188a408e012c6f862fd8
Natalie <natalie.todd15@gmail.com>
2018-08-27 20:46:29 -0600

    front page is looka wise, still needs functionality

---
+++ src/components/Options.vue
@@ -0,0 +1,60 @@
+<template>
+<div class='options'>
+    <div>
+        <b-card 
+          img-src="https://burst.shopifycdn.com/photos/classic-book-with-detailed-cover_373x@2x.progressive.jpg"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 70vw;"
+          class="mb-2">
+    <b-button href="#" variant="primary">Authors</b-button>
+  </b-card>
+        </div>
+            <div>
+        <b-card 
+          img-src="https://burst.shopifycdn.com/photos/hard-cover-books_373x@2x.progressive.jpg"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 70vw;"
+          class="mb-2">
+    <b-button href="#" variant="primary">Books</b-button>
+  </b-card>
+        </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: "Options",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.badge {
+  color: #ea943f;
+}
+
+.options {
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    /* align-content: center; */
+    justify-content: center;
+    align-items: center;
+}
+
+.btn {
+  background-color: #ea943f;  
+  border-color: black;
+}
+
+.card-body {
+    display: flex;
+    justify-content: center;
+}
+</style>
