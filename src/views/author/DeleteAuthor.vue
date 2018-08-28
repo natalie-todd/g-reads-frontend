6ddfb0b703f578a1d22bf8bcdbf56c8146db227b
Natalie <natalie.todd15@gmail.com>
2018-08-28 00:13:29 -0600

    switching to backend

---
+++ src/views/author/DeleteAuthor.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <router-view></router-view>
+    <Header />
+    <Nav />
+    <DeleteA />
+</div>
+</template>
+    
+<script>
+import Header from '@/components/Header';
+import Nav from '@/components/Nav';
+import DeleteA from '@/components/DeleteA';
+export default {
+    components: {
+        Header,
+        Nav,
+        DeleteA
+    }
+};
+</script>
+
+<style>
+</style>
